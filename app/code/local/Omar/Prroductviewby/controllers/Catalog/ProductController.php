ea95de7a63e04cd368863dd6b6d91a0f7bac5e47
Omar Faruk Sharif <omar@omar-sharif.net>
2016-11-24 13:48:50 +0600

    Initial Commit

---
+++ app/code/local/Omar/Prroductviewby/controllers/Catalog/ProductController.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once Mage::getModuleDir('controllers', 'Mage_Catalog').DS.'ProductController.php';
+
+/**
+ * Product controller
+ *
+ * @category   Mage
+ * @package    Mage_Catalog
+ */
+class Omar_Prroductviewby_Catalog_ProductController extends Mage_Catalog_ProductController
+{
+
+
+    /**
+     * Product view action
+     */
+    public function viewbyAction()
+    {
+        // Get initial data from request
+        $categoryId = (int) $this->getRequest()->getParam('category', false);
+
+        //echo '<pre>'; print_r( $this->getRequest()->getParams() ); die();
+
+        $params = $this->getRequest()->getParams();
+
+        if( !empty($params) ) {
+            $param_keys = array_keys($params);
+            if( !empty($param_keys[0]) ) {
+                $param_attr_key = $param_keys[0];
+                $param_attr_val = $params[ $param_attr_key ];
+            }
+        }
+
+        if( empty($param_attr_key) && empty($param_attr_val) ) {
+            $this->_forward('noRoute');
+        } else {
+            $product = Mage::getModel('catalog/product')->loadByAttribute( $param_attr_key , $param_attr_val);
+            if( !empty($product->getId()) )
+            {
+                $productId = $product->getId();
+            }
+            else {
+                $this->_forward('noRoute');
+            }
+        }
+
+        $specifyOptions = $this->getRequest()->getParam('options');
+
+        // Prepare helper and params
+        $viewHelper = Mage::helper('catalog/product_view');
+
+        $params = new Varien_Object();
+        $params->setCategoryId($categoryId);
+        $params->setSpecifyOptions($specifyOptions);
+
+        // Render page
+        try {
+            // Let's make Magento fool about
+            $this->getRequest()->setActionName('view')->setParam('id', $productId);;
+            $viewHelper->prepareAndRender($productId, $this, $params);
+        } catch (Exception $e) {
+            if ($e->getCode() == $viewHelper->ERR_NO_PRODUCT_LOADED) {
+                if (isset($_GET['store'])  && !$this->getResponse()->isRedirect()) {
+                    $this->_redirect('');
+                } elseif (!$this->getResponse()->isRedirect()) {
+                    $this->_forward('noRoute');
+                }
+            } else {
+                Mage::logException($e);
+                $this->_forward('noRoute');
+            }
+        }
+    }
+
+}
